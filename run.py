61a5dd1a155c8193c34c791b44ac55338189beb5
Sebastian Hörl <hoerl.sebastian@gmail.com>
2018-10-02 10:21:34 +0200

    Continuous Testing works!

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import tqdm
 import pipeline
 import yaml
 import sys
@@ -9,6 +10,14 @@ if len(sys.argv) > 1:
 
 with open(config_path) as f:
     config = yaml.load(f)
+
+if "disable_progress_bar" in config and config["disable_progress_bar"]:
+    def noop(iterator, desc = "  Running something without description ...", *a, **k):
+        print(desc)
+        return iterator
+
+    tqdm.tqdm = noop
+
 
 pipeline.run(
     config["stages"],
