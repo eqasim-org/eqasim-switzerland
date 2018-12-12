6287a1aa14feff33fe5533f5ac9b81c788bbcb4b
Sebastian Hörl <hoerl.sebastian@gmail.com>
2018-12-12 11:34:05 +0100

    Diversify departure times

--- population/trips.py
+++ population/trips.py
@@ -39,6 +39,20 @@ def execute(context):
 
     df_trips = df_trips[[
         "person_id", "trip_id", "departure_time", "arrival_time", "travel_time", "mode", "following_purpose", "is_commute"
-    ]]
+    ]].sort_values(by = ["person_id", "trip_id"])
+
+    # Diversify departure times
+    counts = df_trips[["person_id", "trip_id"]].groupby("person_id").size().reset_index(name = "count")["count"].values
+
+    interval = df_trips[["person_id", "departure_time"]].groupby("person_id").min().reset_index()["departure_time"].values
+    interval = np.minimum(1800.0, interval) # If first departure time is just 5min after midnight, we only add a deviation of 5min
+
+    offset = np.random.random(size = (len(counts), )) * interval * 2.0 - interval
+    offset = np.repeat(offset, counts)
+
+    df_trips["departure_time"] += offset
+    df_trips["arrival_time"] += offset
+    df_trips["departure_time"] = np.round(df_trips["departure_time"])
+    df_trips["arrival_time"] = np.round(df_trips["arrival_time"])
 
     return df_trips
