06992b027923fe405b0b059638d39582fc496ff7
Sebastian Hörl <hoerl.sebastian@gmail.com>
2019-01-25 00:30:54 +0100

    Add ÖV Güteklasse

--- matsim/writers.py
+++ matsim/writers.py
@@ -7,8 +7,8 @@ import numpy as np
 class XmlWriter:
     def __init__(self, writer):
         self.writer = writer
+        self.scope = []
         self.indent = 0
-        self.scope = None
 
     def _write_line(self, content):
         self._write_indent()
@@ -20,12 +20,26 @@ class XmlWriter:
     def _write(self, content):
         self.writer.write(bytes(content, "utf-8"))
 
-    def _require_scope(self, scope):
-        if scope == None and not self.scope is None:
-            raise RuntimeError("Execpted initial scope")
+    def _require_scope(self, expected_scope):
+        if expected_scope is None:
+            if not len(self.scope) == 0:
+                raise RuntimeError("Execpted initial scope")
+            else:
+                return
+        else:
+            if not type(expected_scope) == tuple and not type(expected_scope) == list:
+                expected_scope = [expected_scope]
 
-        if not self.scope == scope:
-            raise RuntimeError("Expected different scope")
+            if len(self.scope) == 0 or not self.scope[-1] in expected_scope:
+                raise RuntimeError("Expected different scope")
+
+    def _push_scope(self, scope):
+        self.scope.append(scope)
+        self.indent += 1
+
+    def _pop_scope(self):
+        del self.scope[-1]
+        self.indent -= 1
 
     def yes_no(self, value):
         return "yes" if value else "no"
@@ -46,50 +60,47 @@ class XmlWriter:
 
 class PopulationWriter(XmlWriter):
     POPULATION_SCOPE = 0
-    FINISHED_SCOPE = 1
+    # FINISHED_SCOPE = 1
     PERSON_SCOPE = 2
     PLAN_SCOPE = 3
     ATTRIBUTES_SCOPE = 4
+    ACTIVITY_SCOPE = 5
 
     def __init__(self, writer):
         XmlWriter.__init__(self, writer)
 
     def start_population(self):
         self._require_scope(None)
+
         self._write_line('<?xml version="1.0" encoding="utf-8"?>')
         self._write_line('<!DOCTYPE population SYSTEM "http://www.matsim.org/files/dtd/population_v6.dtd">')
         self._write_line('<population desc="Switzerland Baseline">')
 
-        self.scope = self.POPULATION_SCOPE
-        self.indent += 1
+        self._push_scope(self.POPULATION_SCOPE)
 
     def end_population(self):
         self._require_scope(self.POPULATION_SCOPE)
         self._write_line('</population>')
-        self.scope = self.FINISHED_SCOPE
+        self._pop_scope()
 
     def start_person(self, person_id):
         self._require_scope(self.POPULATION_SCOPE)
         self._write_line('<person id="%d">' % person_id)
-        self.scope = self.PERSON_SCOPE
-        self.indent += 1
+        self._push_scope(self.PERSON_SCOPE)
 
     def end_person(self):
         self._require_scope(self.PERSON_SCOPE)
-        self.indent -= 1
-        self.scope = self.POPULATION_SCOPE
+        self._pop_scope()
         self._write_line('</person>')
 
     def start_attributes(self):
-        self._require_scope(self.PERSON_SCOPE)
+        self._require_scope([self.PERSON_SCOPE, self.ACTIVITY_SCOPE])
         self._write_line('<attributes>')
-        self.indent += 1
-        self.scope = self.ATTRIBUTES_SCOPE
+        self._push_scope(self.ATTRIBUTES_SCOPE)
 
     def end_attributes(self):
         self._require_scope(self.ATTRIBUTES_SCOPE)
-        self.indent -= 1
-        self.scope = self.PERSON_SCOPE
+        self._pop_scope()
         self._write_line('</attributes>')
 
     def add_attribute(self, name, type, value):
@@ -101,18 +112,14 @@ class PopulationWriter(XmlWriter):
     def start_plan(self, selected):
         self._require_scope(self.PERSON_SCOPE)
         self._write_line('<plan selected="%s">' % self.yes_no(selected))
-        self.indent += 1
-        self.scope = self.PLAN_SCOPE
+        self._push_scope(self.PLAN_SCOPE)
 
     def end_plan(self):
         self._require_scope(self.PLAN_SCOPE)
-        self.indent -= 1
-        self.scope = self.PERSON_SCOPE
+        self._pop_scope()
         self._write_line('</plan>')
 
-    def add_activity(self, type, location, start_time = None, end_time = None):
-        self._require_scope(self.PLAN_SCOPE)
-
+    def _start_activity(self, type, location, start_time = None, end_time = None):
         self._write_indent()
         self._write('<activity ')
         self._write('type="%s" ' % type)
@@ -120,6 +127,22 @@ class PopulationWriter(XmlWriter):
         if location[2] is not None: self._write('facility="%s" ' % str(location[2]))
         if start_time is not None: self._write('start_time="%s" ' % self.time(start_time))
         if end_time is not None: self._write('end_time="%s" ' % self.time(end_time))
+
+    def start_activity(self, type, location, start_time = None, end_time = None):
+        self._require_scope(self.PLAN_SCOPE)
+        self._start_activity(type, location, start_time, end_time)
+        self._write('>\n')
+        self._push_scope(self.ACTIVITY_SCOPE)
+
+    def end_activity(self):
+        self._require_scope(self.ACTIVITY_SCOPE)
+        self._pop_scope()
+        self._write_line('</activity>')
+
+    def add_activity(self, type, location, start_time = None, end_time = None):
+        self._require_scope(self.PLAN_SCOPE)
+
+        self._start_activity(type, location, start_time, end_time)
         self._write('/>\n')
 
     def add_leg(self, mode, departure_time, travel_time):
@@ -145,37 +168,31 @@ class HouseholdsWriter(XmlWriter):
         self._require_scope(None)
         self._write_line('<?xml version="1.0" encoding="utf-8"?>')
         self._write_line('<households xmlns="http://www.matsim.org/files/dtd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.matsim.org/files/dtd http://www.matsim.org/files/dtd/households_v1.0.xsd">')
-
-        self.scope = self.HOUSEHOLDS_SCOPE
-        self.indent += 1
+        self._push_scope(self.HOUSEHOLDS_SCOPE)
 
     def end_households(self):
         self._require_scope(self.HOUSEHOLDS_SCOPE)
         self._write_line('</households>')
-        self.scope = self.FINISHED_SCOPE
+        self._pop_scope()
 
     def start_household(self, household_id):
         self._require_scope(self.HOUSEHOLDS_SCOPE)
         self._write_line('<household id="%d">' % household_id)
-        self.scope = self.HOUSEHOLD_SCOPE
-        self.indent += 1
+        self._push_scope(self.HOUSEHOLD_SCOPE)
 
     def end_household(self):
         self._require_scope(self.HOUSEHOLD_SCOPE)
-        self.indent -= 1
-        self.scope = self.HOUSEHOLDS_SCOPE
+        self._pop_scope()
         self._write_line('</household>')
 
     def start_attributes(self):
         self._require_scope(self.HOUSEHOLD_SCOPE)
         self._write_line('<attributes>')
-        self.indent += 1
-        self.scope = self.ATTRIBUTES_SCOPE
+        self._push_scope(self.ATTRIBUTES_SCOPE)
 
     def end_attributes(self):
         self._require_scope(self.ATTRIBUTES_SCOPE)
-        self.indent -= 1
-        self.scope = self.HOUSEHOLD_SCOPE
+        self._pop_scope()
         self._write_line('</attributes>')
 
     def add_attribute(self, name, type, value):
@@ -209,28 +226,23 @@ class FacilitiesWriter(XmlWriter):
         self._write_line('<?xml version="1.0" encoding="utf-8"?>')
         self._write_line('<!DOCTYPE facilities SYSTEM "http://www.matsim.org/files/dtd/facilities_v1.dtd">')
         self._write_line('<facilities name="Facilities from different sources">')
-
-        self.scope = self.FACILITIES_SCOPE
-        self.indent += 1
+        self._push_scope(self.FACILITIES_SCOPE)
 
     def end_facilities(self):
         self._require_scope(self.FACILITIES_SCOPE)
         self._write_line('</facilities>')
-        self.scope = self.FINISHED_SCOPE
+        self._pop_scope()
 
     def start_facility(self, facility_id, x, y):
         self._require_scope(self.FACILITIES_SCOPE)
         self._write_line('<facility id="%s" x="%f" y="%f">' % (
             str(facility_id), x, y
         ))
-
-        self.indent += 1
-        self.scope = self.FACILITY_SCOPE
+        self._push_scope(self.FACILITY_SCOPE)
 
     def end_facility(self):
         self._require_scope(self.FACILITY_SCOPE)
-        self.indent -= 1
-        self.scope = self.FACILITIES_SCOPE
+        self._pop_scope()
         self._write_line('</facility>')
 
     def add_activity(self, purpose):
