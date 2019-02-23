4bb716119fccf8d98df6a787ea7cc257cacfa872
Sebastian Hörl <hoerl.sebastian@gmail.com>
2019-02-23 17:50:27 +0100

    Add CSV output for MZ

---
+++ data/microcensus/csv.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import data.utils
+import data.spatial.utils
+import data.constants as c
+import pyproj
+import geopandas as gpd
+import data.spatial.ov_guteklasse
+
+def configure(context, require):
+    require.stage("data.microcensus.persons")
+    require.stage("data.microcensus.trips")
+
+def execute(context):
+    df_persons = context.stage("data.microcensus.persons")
+    df_trips = context.stage("data.microcensus.trips")
+
+    df_persons.to_csv("%s/persons.csv" % context.cache_path, sep = ";", index = None)
+    df_trips.to_csv("%s/trips.csv" % context.cache_path, sep = ";", index = None)
