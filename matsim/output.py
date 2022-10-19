074a8a7533b415bfb12b870b6dc0927329bffb81
Christopher Tchervenkov <tchervec@ethz.ch>
2022-10-19 11:38:06 +0200

    copy files from matsim.simulation.prepare to final matsim scenario

--- matsim/output.py
+++ matsim/output.py
@@ -4,22 +4,23 @@ import shutil
 
 def configure(context):
     context.stage("matsim.simulation.run")
-    context.config("output_path")
-    context.config("output_id")
+    context.stage("matsim.simulation.prepare")
     context.stage("contracts.contracts")
     
-    context.config("input_prefix", "output_")
+    context.config("output_path")
+    context.config("output_id")    
     context.config("output_prefix", "switzerland_")
 
-def execute(context):
-    results_path = "%s/simulation_output" % context.stage("matsim.simulation.run")
 
+def execute(context):
+    source_path = context.path("matsim.simulation.prepare")
     output_path = context.config("output_path")
     output_id = context.config("output_id")
 
     if not os.path.isdir(output_path):
         raise RuntimeError("Output path does not exist:", output_path)
 
+    # create target path
     target_path = "%s/%s" % (output_path, output_id)
 
     if os.path.exists(target_path):
@@ -30,19 +31,23 @@ def execute(context):
             raise RuntimeError("Cannot clean target path:", target_path)
 
     os.mkdir(target_path)
+    
+    # copy scenario files to from source path target path
+    file_names = [
+        "%shouseholds.xml.gz" % context.config("output_prefix"),
+        "%spopulation.xml.gz" % context.config("output_prefix"),
+        "%sfacilities.xml.gz" % context.config("output_prefix"),
+        "%snetwork.xml.gz" % context.config("output_prefix"),
+        "%stransit_schedule.xml.gz" % context.config("output_prefix"),
+        "%stransit_vehicles.xml.gz" % context.config("output_prefix"),
+        "%sconfig.xml" % context.config("output_prefix")
+    ]
+    
+    for file in file_names:
+        shutil.copyfile("%s/%s" % (source_path, file), 
+                        "%s/%s" % (target_path, file))
 
-    for file in [
-        "network.xml.gz",
-        "transitSchedule.xml.gz",
-        "transitVehicles.xml.gz",
-        "facilities.xml.gz",
-        "households.xml.gz",
-        "plans.xml.gz",
-        "config.xml"
-    ]:
-        shutil.copyfile("%s/%s%s" % (results_path, context.config("input_prefix"), file), 
-                        "%s/%s%s" % (target_path, context.config("output_prefix"), file))
-
+    # copy contract information
     contracts_path = context.stage("contracts.contracts")
     shutil.copyfile(contracts_path, "%s/CONTRACTS.html" % target_path)
 
