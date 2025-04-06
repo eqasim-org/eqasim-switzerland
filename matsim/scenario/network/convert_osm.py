2b68ad3de28906996e02e182591811b2ab44c58d
dabdelkader <dabdelkader@ethz.ch>
2025-04-06 23:09:24 +0200

    Converting osm.pbf to .osm using osmosis or pyosmium.

--- matsim/scenario/network/convert_osm.py
+++ matsim/scenario/network/convert_osm.py
@@ -1,15 +1,90 @@
 import os
-
+import matsim.scenario.network.osmosis
 import matsim.runtime.pt2matsim as pt2matsim
+import sys
+import osmium
+import shutil
 
 
 def configure(context):
     context.stage("matsim.runtime.java")
     context.stage("matsim.runtime.pt2matsim")
+    context.stage("matsim.scenario.network.osmosis")
+    
     context.config("data_path")
+    context.config("osm_path", "switzerland-latest.osm.gz")
+    context.config("export_detailed_network", False)
+
+
+###############################################################
+# Functions used to convert the network file:
+
+class OSMHandler(osmium.SimpleHandler):
+    def __init__(self, writer):
+        super(OSMHandler, self).__init__()
+        self.writer = writer
+
+    def node(self, n):
+        self.writer.add_node(n)
+
+    def way(self, w):
+        self.writer.add_way(w)
+
+    def relation(self, r):
+        self.writer.add_relation(r)
+
+def convert_pbf_to_osm_pyosmium(input_file, output_file):
+    print("using pyosmium to convert .pbf data")
+    if os.path.exists(output_file):
+        print("The file: %s already exists. It will be overridden." % output_file)
+        os.remove(output_file)
+
+    writer = osmium.SimpleWriter(output_file)
+    handler = OSMHandler(writer)
+
+    try:
+        print(f"Processing {input_file} → {output_file} ...")
+        handler.apply_file(input_file)
+        print(f"Conversion successful: {output_file}")
+    except Exception as e:
+        print(f"Error during network conversion using pyosmium: {e}")
+        sys.exit(1)
+    finally:
+        writer.close()
+
+def convert_pbf_to_osm_osmosis(context, input_file, output_file):
+    print("using osmosis to convert .pbf data")
+    if os.path.exists(output_file):
+        print("The file: %s already exists. It will be overridden." % output_file)
+        os.remove(output_file)
+    
+    try:
+        matsim.scenario.network.osmosis.run(context, [
+                "--read-pbf", input_file,
+                "--tag-filter", "accept-ways", "highway=*", "railway=*",
+                "completeWays=yes", "--used-node",
+                "--write-xml", "compressionMethod=gzip", output_file
+            ])
+    except Exception as e:
+        print(f"Error during network conversion using osmosis: {e}")
+        sys.exit(1)
+
+############################################################## 
 
 
 def execute(context):
+    osm_file = '%s/osm/%s' % (context.config("data_path"), context.config("osm_path"))
+
+    if context.config("osm_path").endswith('.pbf'):
+        osmosis_path = shutil.which("osmosis")
+        if osmosis_path:
+            new_file_name = osm_file[:-8]+"-osmosis.osm.gz"
+            convert_pbf_to_osm_osmosis(context, osm_file, new_file_name)
+        else:
+            new_file_name = osm_file[:-8]+"-pyosmium.osm"
+            convert_pbf_to_osm_pyosmium(osm_file, new_file_name)
+
+        osm_file = new_file_name
 
     pt2matsim.run(context, "org.matsim.pt2matsim.run.CreateDefaultOsmConfig", [
         "convert_network_template.xml"
@@ -21,8 +96,8 @@ def execute(context):
 
         content = content.replace(
             '<param name="osmFile" value="null" />',
-            '<param name="osmFile" value="%s/osm/switzerland-latest.osm.gz" />' % context.config("data_path")
-        )
+            '<param name="osmFile" value="%s" />' % osm_file
+            )
         
         content = content.replace(
             '<param name="outputCoordinateSystem" value="null" />',
@@ -33,6 +108,18 @@ def execute(context):
             '<param name="outputNetworkFile" value="null" />',
             '<param name="outputNetworkFile" value="%s/converted_network.xml.gz" />' % context.path()
         )
+        
+        # higher link length
+        content = content.replace(
+            '<param name="maxLinkLength" value="500.0" />',
+            '<param name="maxLinkLength" value="1000.0" />'
+        )
+        # Export detailed geometry of the links if needed
+        if context.config("export_detailed_network"):
+            content = content.replace(
+                '<param name="outputDetailedLinkGeometryFile" value="null" />',
+                '<param name="outputDetailedLinkGeometryFile" value="detailed_network.csv" />',
+            )
 
         content = content.replace(
             '</module>',
