81fe50c59bbcc13252c9e3f676f783ab7dffd0ee
dabdelkader <dabdelkader@ethz.ch>
2025-05-16 16:37:57 +0200

    Keep car network connectivity after turning on turnrestrictions (modif in clean network function).

--- matsim/readers.py
+++ matsim/readers.py
@@ -114,17 +114,19 @@ class Network:
         df = df.drop_duplicates(subset=['length','modes','from_node', 'to_node', 'capacity'],
                                 ignore_index=True)
         stats["removed_link_duplicates"] = len_df-len(df)
-        print("There are %d link duplicates in the network that are removed." % stats["removed_link_duplicates"] )
-        
-        # Removed links that are not connected to the whole graph
-        print("Remove unconnected links")
-        df, num_removed = self.remove_unconnected_links(df)
-        stats["removed_unconnected_links"] = num_removed
+        print("There are %d link duplicates in the network that are removed." % stats["removed_link_duplicates"] )                
         
         # Removing nodes with no intersection
         print("Removing nodes that do not represent an intersection.")
         df, stats2 = self.merge_link_chains(df)
-        # df = pd.concat([df,df_rest], ignore_index=True)
+        
+        # Removed links that are not connected to the whole graph
+        print("Remove unconnected links")
+        cond = "disallowedNextLinks" in self.link_attrs["name"].unique()
+        func = self.remove_unconnected_links_with_turns_restrictions if cond else self.remove_unconnected_links
+        df, num_removed = func(df)        
+        stats["removed_unconnected_links"] = num_removed
+
         self.links = df
         stats.update(stats2)
         
@@ -141,8 +143,7 @@ class Network:
             warnings.warn("Attributes are not present in the links dataframe.", category=UserWarning)
         
         return stats
-    
-    @classmethod
+        
     def remove_unconnected_links(self, df):        
         """
         Removes links that are not part of the largest connected component of the road network.
@@ -176,9 +177,61 @@ class Network:
         
         df = pd.concat([connected_links, df_other], ignore_index=True)
         return df, len(sel)-len(df)
-    
+        
+    def remove_unconnected_links_with_turns_restrictions(self, df):
+        """
+        Removes links not part of the largest connected component for cars, considering turn restrictions.
+        In this case, the links are the nodes of the created graph.
+        Parameters:
+            df (pd.DataFrame): Road network with columns ['from_node', 'to_node', 'link_id', 'modes', 'disallowedNextLinks']
+        
+        Returns:
+            pd.DataFrame: Filtered DataFrame with connected car links only.
+        """
 
-    @classmethod
+        import networkx as nx        
+        df = df.copy()
+        
+        # Filter to car-accessible links
+        sel = df["modes"].apply(lambda x: "car" in x)
+        df_other = df[~sel].copy()
+        df = df[sel].reset_index(drop=True)
+
+        print("    Building turn-aware connectivity graph...")
+
+        # Create a mapping from to_node to list of incoming links
+        links_from_node = df.groupby('from_node')['link_id'].apply(list).to_dict()
+        link_info = df.set_index('link_id')
+
+        # Build a graph where nodes are link_ids, and edges represent allowed turns
+        G = nx.DiGraph()
+        for link_id, row in link_info.iterrows():
+            to_node = row['to_node']
+            disallowed = set()          
+            disallowed_links = row.attributes.get("disallowedNextLinks", {})
+            if isinstance(disallowed_links, str):
+                disallowed_links = eval(disallowed_links)
+            if "car" in disallowed_links:                
+                disallowed.update([j for i in disallowed_links["car"] for j in i])
+
+            # Get all links starting from this to_node (i.e., possible successors)
+            next_links = links_from_node.get(to_node, [])
+            for next_link_id in next_links:
+                if next_link_id not in disallowed:
+                    G.add_edge(link_id, next_link_id)
+
+        # Get largest weakly connected component
+        largest_cc = max(nx.weakly_connected_components(G), key=len)
+
+        # Filter to only links part of the largest component
+        connected_links = df[df['link_id'].isin(largest_cc)]
+
+        # Add back non-car links
+        df_final = pd.concat([connected_links, df_other], ignore_index=True)
+
+        removed_count = len(df) - len(connected_links)
+        return df_final, removed_count
+
     def merge_link_chains(self, df):
         import networkx as nx #only if we use this function
         stats = {"number_of_nodes":0,
@@ -252,7 +305,7 @@ class Network:
                 current_chain = [idx]
                 current_node = succ
                 current_attrs = df.loc[idx, ['link_id', 'freespeed', 'capacity', 'permlanes',
-                                             'oneway', 'modes','attributes']].to_dict()
+                                             'oneway', 'modes']].to_dict()
 
                 while is_degree2(current_node):
                     next_nodes = list(G.successors(current_node))
@@ -296,6 +349,7 @@ class Network:
                             'length': chain_rows['length'].sum(),
                             **current_attrs #we use the link_id of the first link
                         }
+                        new_link["attributes"] = df.loc[current_chain[-1], 'attributes'] #should keep the attributes of last link, in case of turn restrictions
                         new_link["attributes"]["old_link_id"] = "_".join(chain_rows['link_id'].tolist())
                         merged_links.append(new_link)
                         visited_links.update(current_chain)
