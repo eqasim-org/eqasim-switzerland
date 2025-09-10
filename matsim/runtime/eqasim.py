221042349c881c317cf486859cb36fee498f4e23
Aurore Sallard <asallard@eu-login-37.euler.ethz.ch>
2025-09-10 11:32:53 +0200

    Updating eqasim commit

--- matsim/runtime/eqasim.py
+++ matsim/runtime/eqasim.py
@@ -7,7 +7,7 @@ import matsim.runtime.maven as maven
 
 DEFAULT_EQASIM_VERSION = "2.0.0"
 DEFAULT_EQASIM_BRANCH = "cmdp"
-DEFAULT_EQASIM_COMMIT = "acf065f"
+DEFAULT_EQASIM_COMMIT = "4145959"
 
 
 def configure(context):
