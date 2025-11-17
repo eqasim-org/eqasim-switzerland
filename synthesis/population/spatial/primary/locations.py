2ce948c06c96dc6198866c8d7c3c98f142735122
dabdelkader <dabdelkader@ethz.ch>
2025-11-17 11:19:20 +0100

    fix: locations

--- synthesis/population/spatial/primary/locations.py
+++ synthesis/population/spatial/primary/locations.py
@@ -4,7 +4,7 @@ def configure(context):
     if context.config("work_location_algorithm") == "synthesis.population.spatial.primary.work.work_locations":
         context.stage("synthesis.population.spatial.primary.work.work_locations", alias="work_locations")
     else:
-        context.stage("synthesis.population.spatial.primary.education.locations", alias="work_locations")
+        context.stage("synthesis.population.spatial.primary.work.locations", alias="work_locations")
 
     context.stage("synthesis.population.spatial.primary.education.locations", alias="education_locations")
 
