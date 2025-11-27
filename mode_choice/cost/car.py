ce76c95b2eb863913104f524c828890b8f06bba8
dabdelkader <dabdelkader@ethz.ch>
2025-11-27 10:32:14 +0100

    fix: correct car cost

--- mode_choice/cost/car.py
+++ mode_choice/cost/car.py
@@ -6,14 +6,13 @@ We do not use it currently because of pandana current limitations.
 
 import numpy as np
 from mode_choice.dmc_defaults import Defaults
-# for the record:
-# this function can be used as car cost, fit from this paper:
-# Weis, C., Kowald, M., Danalet, A., Schmid, B., Vrtic, M., Axhausen, K.W. and Mathys, N., 2021. Surveying and analysing mode and route choices in Switzerland 2010–2015. Travel behaviour and society, 22, pp.10-21.
-# function:
-# func = lambda x: np.minimum(0.3, 0.104 + 0.6 * np.exp(-1.2*(x**0.33)))
 
 def car_cost_weiss(distance_km, *args, **kwargs):
-    return np.minimum(0.3, 0.104 + 0.6 * np.exp(-1.2*(distance_km**0.33)))
+    # for the record:
+    # this function can be used as car cost, fit from this paper:
+    # Weis, C., Kowald, M., Danalet, A., Schmid, B., Vrtic, M., Axhausen, K.W. and Mathys, N., 2021. Surveying and analysing mode and route choices in Switzerland 2010–2015. Travel behaviour and society, 22, pp.10-21.
+    cost_per_km = np.minimum(0.3, 0.104 + 0.6 * np.exp(-1.2*(distance_km**0.33)))
+    return distance_km * cost_per_km
 
 def car_cost(distance_km, cost_per_km):
     return distance_km * cost_per_km
