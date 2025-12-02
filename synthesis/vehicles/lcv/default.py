6987785402ffb9801642562346d1db8c8db29412
balacm <milos.balac@ivt.baug.ethz.ch>
2025-12-02 10:01:28 +0000

    feat: Add LCV vehicles to synthetic population

---
+++ synthesis/vehicles/lcv/default.py
@@ -0,0 +1,28 @@
+import re
+import pandas as pd
+
+"""
+Creates a vehicle fleet based on a default vehicle type for the dummy passenger mode
+"""
+
+def configure(context):
+    context.stage("synthesis.lcv.trips")
+
+def execute(context):
+    df_lcv = context.stage("synthesis.lcv.trips")
+    df_vehicle_types = pd.DataFrame.from_records([{
+        "type_id": "default_lcv", "nb_seats": 1, "length": 9.0, "width": 1.0, "pce": 2.0, "mode": "lcv",
+        "hbefa_cat": "HEAVY_GOODS_VEHICLE", "hbefa_tech": "average", "hbefa_size": "average", "hbefa_emission": "average", ##TODO: check hbefa_category
+    }])
+
+    df_vehicles = df_lcv[["trip_id"]].copy()
+    df_vehicles = df_vehicles.rename(columns = { "trip_id": "owner_id" })
+    
+    df_vehicles["mode"] = "lcv"
+
+    df_vehicles["vehicle_id"] = df_vehicles["owner_id"].astype(str) + ":lcv"
+    df_vehicles["type_id"] = "default_lcv"
+    df_vehicles["age"] = 0
+    df_vehicles["euro"] = 6
+
+    return df_vehicle_types, df_vehicles
