8f5ecfe638885ee11f0b70c419fa3c4d83d7db53
Dib-AEK <abdelkader.dib@csfm.ethz.ch>
2025-12-05 15:49:45 +0100

    New work location assignement from cmdp branch

--- synthesis/population/spatial/primary/locations.py
+++ synthesis/population/spatial/primary/locations.py
@@ -1,10 +1,15 @@
 def configure(context):
-    context.stage("synthesis.population.spatial.primary.work.locations")
-    context.stage("synthesis.population.spatial.primary.education.locations")
+    context.config("work_location_algorithm", default = "synthesis.population.spatial.primary.work.work_locations")
+    
+    if context.config("work_location_algorithm") == "synthesis.population.spatial.primary.work.work_locations":
+        context.stage("synthesis.population.spatial.primary.work.work_locations", alias="work_locations")
+    else:
+        context.stage("synthesis.population.spatial.primary.work.locations", alias="work_locations")
 
+    context.stage("synthesis.population.spatial.primary.education.locations", alias="education_locations")
 
 def execute(context):
-    df_work = context.stage("synthesis.population.spatial.primary.work.locations")
-    df_education = context.stage("synthesis.population.spatial.primary.education.locations")
+    df_work = context.stage("work_locations")
+    df_education = context.stage("education_locations")
 
     return df_work, df_education
