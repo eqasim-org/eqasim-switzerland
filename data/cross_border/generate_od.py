44d28a7de65d979f7a3e197c39fe26a0012f1bec
Aurore Sallard <asallard@eu-login-43.euler.ethz.ch>
2026-02-23 18:25:31 +0100

    Fixing cross border population weights, now scaled to 2015 counts

--- data/cross_border/generate_od.py
+++ data/cross_border/generate_od.py
@@ -213,7 +213,7 @@ def process_through_trips(through_trips, N, context):
     return df
 
 
-def execute(context):
+def read_2021_data(context):
     # Load data
     # We are using the 2021 release because the 2015 one doesn't provide reliable destination coordinates.
     # Obviously there are some covid-related biases but this is the best we currently have.
@@ -227,13 +227,13 @@ def execute(context):
                  "STARTORTORTLATITUDE", "STARTORTORTLONGITUDE",
                  "ZIELORTORTLATITUDE", "ZIELORTORTLONGITUDE",
                  "FAHRTZWECK", "ANZAHLUEBERNACHTUNGEN", "AUFENTHALTSLAND1ISO", "AUFENTHALTSLAND2ISO", "AUFENTHALTSLAND3ISO",
-                 "ZUGTYP", "FAHRTRICHTUNGGU", "FAHRTRICHTUNGAU", "VERKEHRSART", "GEWICHT_Personen"]]
+                 "ZUGTYP", "FAHRTRICHTUNGGU", "FAHRTRICHTUNGAU", "VERKEHRSART", "GEWICHT_Personen", "GEWICHT_Fahrzeuge"]]
 
     df2021.columns = ["interview_id", "interview_place_id", "interview_place", "neighbor_country", "day_cat", "road_type", "crossing_cat", "vehicle_type", "nb_passengers",
                   "residence_country", "residence_place", "origin_country", "origin_place", "destination_country", "destination_place",
                   "start_y", "start_x", "end_y", "end_x", 
                   "trip_purpose", "nb_nights", "country1", "country2", "country3",
-                  "train_type", "direction_crossing", "direction_alps", "travel_cat", "weight"]
+                  "train_type", "direction_crossing", "direction_alps", "travel_cat", "weight", "weight_vehicles"]
 
     # Process the columns
     # 1. Rename countries
@@ -248,7 +248,7 @@ def execute(context):
     
     # 3. Identify vehicles
     df2021["vehicle_type"] = df2021["vehicle_type"].astype(str)
-    df2021.loc[df2021["vehicle_type"]=="1", "vehicle_type"] = "MIV"
+    df2021.loc[df2021["vehicle_type"]=="1", "vehicle_type"] = "car"
     df2021.loc[df2021["vehicle_type"]=="2", "vehicle_type"] = "car" #"motorcycle"
     df2021.loc[df2021["vehicle_type"]=="3", "vehicle_type"] = "pt"  #"long distance bus"
     
@@ -257,7 +257,7 @@ def execute(context):
     df2021.loc[df2021["trip_purpose"]=="1", "trip_purpose"] = "work"
     df2021.loc[df2021["trip_purpose"]=="2", "trip_purpose"] = "education"
     df2021.loc[df2021["trip_purpose"]=="3", "trip_purpose"] = "shop"
-    df2021.loc[df2021["trip_purpose"]=="4", "trip_purpose"] = "work_secondary"
+    df2021.loc[df2021["trip_purpose"]=="4", "trip_purpose"] = "work"
     df2021.loc[df2021["trip_purpose"]=="5", "trip_purpose"] = "leisure"
     df2021.loc[df2021["trip_purpose"]=="6", "trip_purpose"] = "other"
     df2021.loc[df2021["trip_purpose"]=="7", "trip_purpose"] = "freight"
@@ -291,6 +291,14 @@ def execute(context):
 
     # 10. Aggregate vehicle_type and train_type
     df2021["trip_mode"] = np.where(df2021["vehicle_type"].str.strip() != '', df2021["vehicle_type"], df2021["train_type"])
+
+    df2021["weight"]          = df2021["weight"].astype(float)
+    df2021["weight_vehicles"] = df2021["weight_vehicles"].replace("", 0).replace(" ", 0).fillna(0).astype(float)
+    df2021.loc[df2021["road_type"]=="road", "weight"] = 2 * df2021[df2021["road_type"]=="road"]["weight_vehicles"] 
+    
+    df2021.loc[:, "group_weight"] = df2021["weight"]
+    df2021.loc[df2021["road_type"]=="road", "group_weight"] = df2021[df2021["road_type"]=="road"]["weight"] * df2021[df2021["road_type"]=="road"]["nb_passengers"]
+    df2021["group_weight"] = df2021["group_weight"].astype(float)
     
     del df2021["road_type"]
     del df2021["vehicle_type"]
@@ -314,6 +322,8 @@ def execute(context):
     df_days   = df2021[df2021["day_cat"]==day_key].copy()
     df_days["weight"] = df_days["weight"] / (52 * day_value)
     df_days["weight"] = df_days["weight"] / 2 # Because the persons entering the country have to leave it too
+    df_days["group_weight"] = df_days["group_weight"] / (52 * day_value)
+    df_days["group_weight"] = df_days["group_weight"] / 2
     del df_days["day_cat"]
 
     # 12. Only select border crossing data - remove Alps crossing data
@@ -325,6 +335,155 @@ def execute(context):
     # 13. Remove Swiss residents, their mobility should be covered in the Microcensus
     residents_ch_mask = borders["residence_country"] == "CH"
     borders = borders[~ residents_ch_mask].copy()
+
+    return borders
+
+
+def read_2015_data(context):
+    # Load data
+    # We are using the 2021 release because the 2015 one doesn't provide reliable destination coordinates.
+    # Obviously there are some covid-related biases but this is the best we currently have.
+    data_path = context.config("data_path")
+    data_path = f"{data_path}/crossborder/AuGQPV_2015/Finale_Auswertungsdatenbank_AGQPV2015_V2.csv"
+
+    df2015   = pd.read_csv(data_path, encoding = "latin1", sep = ",")
+
+    df2015   = df2015[["INTERVIEWID", "BEFRAGUNGSORTID", "BEFRAGUNGSORT", "GRENZABSCHNITT", "TAGESTYP", 
+                    "VERKEHRSTRAEGER", "UEBERGANGSART", "FAHRZEUGTYP", "GRUPPENGROESSE", 
+                    "WOHNORTLANDISO", "STARTORTLANDISO", "ZIELORTLANDISO", 
+                    "STARTORTORTLATITUDE", "STARTORTORTLONGITUDE",
+                    "ZIELORTORTLATITUDE","ZIELORTORTLONGITUDE",
+                    "FAHRTZWECK", "ANZAHLUEBERNACHTUNGEN",
+                    "AUFENTHALTSLAND1ISO", "AUFENTHALTSLAND2ISO", "AUFENTHALTSLAND3ISO",
+                    "ZUGTYP", "FAHRTRICHTUNGGU", "FAHRTRICHTUNGAU", "VERKEHRSART", "GEWICHT"]]
+    
+    df2015.columns = ["interview_id", "interview_place_id", "interview_place", "neighbor_country", "day_cat", 
+                  "road_type", "crossing_cat", "vehicle_type", "nb_passengers",
+                  "residence_country", "origin_country", "destination_country", 
+                  "start_y", "start_x", 
+                  "end_y", "end_x", 
+                  "trip_purpose", "nb_nights", 
+                  "country1", "country2", "country3",
+                  "train_type", "direction_crossing", "direction_alps", "travel_cat", "weight"]
+    
+    swiss_neighbors = ['CH', 'FR', 'DE', 'IT', 'AT', 'LI']
+    for column in ["residence_country", "origin_country", "destination_country"]:
+        df2015.loc[:, column] = df2015[column].apply(lambda x: x if x in swiss_neighbors else "other")
+
+    df2015["road_type"] = df2015["road_type"].astype(str)
+    df2015.loc[df2015["road_type"]=="1", "road_type"] = "road"
+    df2015.loc[df2015["road_type"]=="2", "road_type"] = "rail"
+
+    df2015["vehicle_type"] = df2015["vehicle_type"].astype(str)
+    df2015.loc[df2015["vehicle_type"]=="1", "vehicle_type"] = "car"
+    df2015.loc[df2015["vehicle_type"]=="3", "vehicle_type"] = "car"
+    df2015.loc[df2015["vehicle_type"]=="4", "vehicle_type"] = "pt"
+
+    df2015["trip_purpose"] = df2015["trip_purpose"].astype(str)
+    df2015.loc[df2015["trip_purpose"]=="1", "trip_purpose"] = "work"
+    df2015.loc[df2015["trip_purpose"]=="2", "trip_purpose"] = "education"
+    df2015.loc[df2015["trip_purpose"]=="3", "trip_purpose"] = "shop"
+    df2015.loc[df2015["trip_purpose"]=="4", "trip_purpose"] = "work"
+    df2015.loc[df2015["trip_purpose"]=="5", "trip_purpose"] = "leisure"
+    df2015.loc[df2015["trip_purpose"]=="6", "trip_purpose"] = "other"
+    df2015.loc[df2015["trip_purpose"]=="7", "trip_purpose"] = "freight"
+
+    df2015["direction_crossing"] = df2015["direction_crossing"].astype(str)
+    df2015.loc[df2015["direction_crossing"]=="1", "direction_crossing"] = "entering CH"
+    df2015.loc[df2015["direction_crossing"]=="2", "direction_crossing"] = "leaving CH"
+
+    df2015["direction_alps"] = df2015["direction_alps"].astype(str)
+    df2015.loc[df2015["direction_alps"]=="1", "direction_alps"] = "North"
+    df2015.loc[df2015["direction_alps"]=="2", "direction_alps"] = "South"
+
+    df2015["travel_cat"] = df2015["travel_cat"].astype(str)
+    df2015.loc[df2015["travel_cat"]=="1", "travel_cat"] = "Within CH"
+    df2015.loc[df2015["travel_cat"]=="2", "travel_cat"] = "Through CH"
+    df2015.loc[df2015["travel_cat"]=="3", "travel_cat"] = "From CH"
+    df2015.loc[df2015["travel_cat"]=="4", "travel_cat"] = "To CH"
+    df2015.loc[~df2015["travel_cat"].isin(["Within CH", "Through CH", "From CH", "To CH"]), "travel_cat"]  = "Unknown"
+
+    df2015.loc[df2015["day_cat"]=="Werktag", "day_cat"]    = "Mo-Fr"
+    df2015.loc[df2015["day_cat"]=="Samstag", "day_cat"]    = "WE"
+    df2015.loc[df2015["day_cat"]=="Sonntag", "day_cat"]    = "WE"
+    df2015.loc[df2015["day_cat"]=="Wochenende", "day_cat"] = "WE"
+
+    df2015.loc[df2015["train_type"]=="FV", "train_type"] = "pt"
+    df2015.loc[df2015["train_type"]=="RV", "train_type"] = "pt"
+
+    df2015["trip_mode"] = np.where(df2015["vehicle_type"].str.strip() != '', df2015["vehicle_type"], df2015["train_type"])
+
+    days    = {"Mo-Fr": 5, "WE": 2}
+
+    day = context.config("specific_day_scenario")
+
+    if day == "weekend":
+        day_key = "WE"
+    elif day == "workday":
+        day_key = "Mo-Fr"
+    elif day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]:
+        day_key = "Mo-FR"
+    elif day in ["Saturday", "Sunday"]:
+        day_key = "WE"
+
+    day_value = days[day_key]
+    df_days   = df2015[df2015["day_cat"]==day_key].copy()
+    df_days["weight"] = df_days["weight"] / (52 * day_value)
+    df_days["weight"] = df_days["weight"] / 2 # Because the persons entering the country have to leave it too
+    del df_days["day_cat"]
+
+    df2015 = df_days.copy()
+
+    df2015.loc[df2015["road_type"]=="road", "weight"] = df2015[df2015["road_type"]=="road"]["weight"] * 2
+    df2015.loc[:, "group_weight"] = df2015["weight"] 
+    df2015.loc[df2015["road_type"]=="road", "group_weight"] = df2015[df2015["road_type"]=="road"]["weight"] * df2015[df2015["road_type"]=="road"]["nb_passengers"]
+
+    del df2015["vehicle_type"]
+    del df2015["train_type"]
+
+    borders2015 = df2015[df2015["crossing_cat"]==1]
+
+    del borders2015["crossing_cat"]
+    del borders2015["direction_alps"]
+
+    residents_ch_mask = borders2015["residence_country"] == "CH"
+    borders2015 = borders2015[~ residents_ch_mask].copy()
+
+    return borders2015
+
+
+def execute(context):
+    borders2021 = read_2021_data(context)    
+    borders2015 = read_2015_data(context)
+
+    grouped2021 = borders2021.groupby(["trip_mode", "trip_purpose", "origin_country", "destination_country"], as_index = False)["group_weight"].sum().rename(columns = {"group_weight": "group_weight_2021"})
+    grouped2015 = borders2015.groupby(["trip_mode", "trip_purpose", "origin_country", "destination_country"], as_index = False)["group_weight"].sum().rename(columns = {"group_weight": "group_weight_2015"})
+
+    grouped = grouped2021.merge(grouped2015, how = "left", on = ["trip_mode", "trip_purpose", "origin_country", "destination_country"])
+
+    grouped.loc[grouped["group_weight_2015"].isna(), "scaling_factor"]  = 1
+    grouped.loc[~grouped["group_weight_2015"].isna(), "scaling_factor"] = grouped[~grouped["group_weight_2015"].isna()]["group_weight_2015"] / grouped[~grouped["group_weight_2015"].isna()]["group_weight_2021"]
+    grouped = grouped[["trip_mode", "trip_purpose", "origin_country", "destination_country", "scaling_factor"]]
+
+    borders = borders2021.merge(
+        grouped,
+        on=["trip_mode", "trip_purpose", "origin_country", "destination_country"],
+        how="left"
+    )
+
+    #missing = borders["scaling_factor"].isna().sum()
+    #print(f"Rows without scaling factor: {missing}")
+
+    borders["weight"] = (
+        borders["weight"] *
+        borders["scaling_factor"]
+    )
+
+    borders["group_weight"] = (
+        borders["group_weight"] *
+        borders["scaling_factor"]
+    )
+
 
     # 14. Selector by origin country
     allowed_countries  = ["FR", "DE", "AT", "LI", "IT"]
@@ -393,7 +552,7 @@ def execute(context):
         excluded_ids = df.loc[df["exclude"], "cross_border_person_id"].unique()
         df = df[~df["cross_border_person_id"].isin(excluded_ids)].copy()
 
-        print(df.head())
+    print(len(df))
              
 
     return df
