ad1f6f1b5777fe6c23475b314046794b64043b94
dabdelkader <dabdelkader@ethz.ch>
2026-03-10 17:14:59 +0100

    chore: update eqasim

--- matsim/runtime/eqasim.py
+++ matsim/runtime/eqasim.py
@@ -7,7 +7,7 @@ import matsim.runtime.maven as maven
 
 DEFAULT_EQASIM_VERSION = "2.0.0"
 DEFAULT_EQASIM_BRANCH = "cmdp"
-DEFAULT_EQASIM_COMMIT = "b53e670ae78962476785866f079a410a5f5cd2d4"
+DEFAULT_EQASIM_COMMIT = "121f89e46191768a554ab9ea5302995c5de14151"
 
 
 def configure(context):
