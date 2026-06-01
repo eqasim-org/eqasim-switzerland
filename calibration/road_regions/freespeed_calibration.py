697e40a761522c44c1d39dd147ac0b211702e582
dabdelkader <dabdelkader@ethz.ch>
2026-06-01 21:09:23 +0200

    feay: use of freespeed special region

--- calibration/road_regions/freespeed_calibration.py
+++ calibration/road_regions/freespeed_calibration.py
@@ -1,7 +1,15 @@
+from .penalty_calibration import REGIONS
+import os
+import json
+
 def configure(context):
   pass
 
 def execute(context):
-  return ""
+  path = os.path.join(context.path(), "regions.json")
+  with open(path, "w") as f:
+    json.dump(REGIONS, f)
+  
+  return path
 
 
